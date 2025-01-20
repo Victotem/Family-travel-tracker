5f074f4ac2d153e2c42bfc1b855bf95cacd3e8ea
Victor Javier <88975064+Victotem@users.noreply.github.com>
2025-01-20 23:40:47 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,33 @@
 
+CREATE TABLE student (
+  id SERIAL PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT
+);
+
+-- One to One --
+CREATE TABLE contact_detail (
+  id INTEGER REFERENCES student(id) UNIQUE,
+  tel TEXT,
+  address TEXT
+);
+
+-- Data --
+INSERT INTO student (first_name, last_name)
+VALUES ('Angela', 'Yu');
+INSERT INTO contact_detail (id, tel, address)
+VALUES (1, '+123456789', '123 App Brewery Road');
+
+-- Data --
+INSERT INTO homework_submission (mark, student_id)
+VALUES (98, 1), (87, 1), (88, 1)
+
+-- Data --
+INSERT INTO student (first_name, last_name)
+VALUES ('Jack', 'Bauer');
+
+INSERT INTO class (title)
+VALUES ('English Literature'), ('Maths'), ('Physics');
+
+INSERT INTO enrollment (student_id, class_id ) VALUES (1, 1), (1, 2);
+INSERT INTO enrollment (student_id ,class_id) VALUES (2, 2), (2, 3);
